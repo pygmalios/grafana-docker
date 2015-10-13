ec06ae3ac53c592da194b080d3755d22c915fcd1
Jan Antala <jan@antala.sk>
2015-10-13 17:03:45 +0200

    Require angular

--- public/pygmalios/script.js
+++ public/pygmalios/script.js
@@ -1,26 +1,29 @@
-(function(){
+define(['angular'], function(angular){
+  
+  (function(){
 
-var setTimeRange = function() {
+    var setTimeRange = function() {
 
-  var timeRange = angular.element(document.body).injector().get('timeSrv').timeRange();
-  var query = '?start_datetime=' + new Date(timeRange.from._d).toISOString() + '&end_datetime=' + new Date(timeRange.to._d).toISOString();
-  console.log(query);
+      var timeRange = angular.element(document.body).injector().get('timeSrv').timeRange();
+      var query = '?start_datetime=' + new Date(timeRange.from._d).toISOString() + '&end_datetime=' + new Date(timeRange.to._d).toISOString();
+      console.log(query);
 
-  [].forEach.call(document.querySelectorAll('iframe'), function(iframe) {
-    var src = iframe.getAttribute('src').split('?')[0];
-    iframe.setAttribute('src', src + query);
-  });
-};
+      [].forEach.call(document.querySelectorAll('iframe'), function(iframe) {
+        var src = iframe.getAttribute('src').split('?')[0];
+        iframe.setAttribute('src', src + query);
+      });
+    };
 
-angular.element(document).ready(function () {
+    angular.element(document).ready(function () {
 
-  setTimeRange();
-  var $rootScope = angular.element(document.querySelector('[ng-app],[data-ng-app]') || document).scope();
+      setTimeRange();
+      var $rootScope = angular.element(document.querySelector('[ng-app],[data-ng-app]') || document).scope();
 
-  $rootScope.onAppEvent('time-range-changed', function(e, time) {
-    console.log('time-range-changed');
-    setTimeRange();
-  }, $rootScope);
+      $rootScope.onAppEvent('time-range-changed', function(e, time) {
+        console.log('time-range-changed');
+        setTimeRange();
+      }, $rootScope);
+    });
+
+  })();
 });
-
-})();
