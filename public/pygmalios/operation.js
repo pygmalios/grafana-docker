2a438489f91b028ca9e36e4b979985c244bab2f1
Jan Antala <jan@antala.sk>
2015-10-26 16:48:06 +0100

    Add operation_id

--- public/pygmalios/operation.js
+++ public/pygmalios/operation.js
@@ -3,9 +3,9 @@ window.pygmalios = window.pygmalios || {};
 window.pygmalios.getOperation = function(grafanaOrganizationId) {
 
     switch (grafanaOrganizationId) {
-        case 0: return '54ff1f160d5af210068693e9';
         case 1: return '54ff1f160d5af210068693e9';
         case 2: return '54ff1f160d5af210068693e9';
+        case 3: return '54ff1f160d5af210068693e9';
         default: return '';
     }
 };
