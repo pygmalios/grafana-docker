26adf3d21eee92a13101142bc80ef8dcfb68831f
Jan Antala <jan@antala.sk>
2015-10-26 16:32:55 +0100

    Add case to pygmalios operation script

--- public/pygmalios/operation.js
+++ public/pygmalios/operation.js
@@ -3,9 +3,9 @@ window.pygmalios = window.pygmalios || {};
 window.pygmalios.getOperation = function(grafanaOrganizationId) {
 
     switch (grafanaOrganizationId) {
-        0: return '54ff1f160d5af210068693e9';
-        1: return '54ff1f160d5af210068693e9';
-        2: return '54ff1f160d5af210068693e9';
+        case 0: return '54ff1f160d5af210068693e9';
+        case 1: return '54ff1f160d5af210068693e9';
+        case 2: return '54ff1f160d5af210068693e9';
         default: return '';
     }
 };
