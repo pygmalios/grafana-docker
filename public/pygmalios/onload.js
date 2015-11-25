4488605733a6e176cd863f8f91531a089fccf0f8
Jan Antala <jan@antala.sk>
2015-11-25 16:37:03 +0100

    Add onload script timeout

--- public/pygmalios/onload.js
+++ public/pygmalios/onload.js
@@ -1,8 +1,15 @@
 (function(){
 
-    [].forEach.call(document.querySelectorAll('.dashboardmenu-item .sidemenu-item-text'), function(element) {
-        var title = element.innerHTML;
-        element.innerHTML = title.replace(/.*[.][ ]/, '');
-    });
+    var updateSideDashboardMenu = function(){
+        [].forEach.call(document.querySelectorAll('.dashboardmenu-item .sidemenu-item-text'), function(element) {
+            var title = element.innerHTML;
+            element.innerHTML = title.replace(/.*[.][ ]/, '');
+        });
+    };
 
+    window.setTimeout(function(){
+        console.log('pygmalios onload script');
+
+        updateSideDashboardMenu();
+    }, 5000);
 })();
